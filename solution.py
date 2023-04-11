99680828c484838953cd3daedb91cdfa4156066c
daryabaldina1702 <71131519+daryabaldina1702@users.noreply.github.com>
2023-04-11 22:58:20 +0300

    First solution update

--- solution.py
+++ solution.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
-from statsmodels.stats.proportion import proportions_ztest
-
+from scipy.stats import norm
 
 chat_id = 834411281 # Ваш chat ID, не меняйте название переменной
 
@@ -9,7 +8,10 @@ def solution(x_success: int,
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-    alpha = 0.09
-    p = proportions_ztest([x_success, y_success], [x_cnt, y_cnt], alternative='smaller')[1]
-    
-    return p <= alpha 
+    alpha = 0.05
+    p1 = x_success / x_cnt
+    p2 = y_success / y_cnt
+    se = ((p1*(1-p1))/x_cnt + (p2*(1-p2))/y_cnt)**0.5
+    z = (p1 - p2) / se
+    critical_value = abs(norm.ppf(alpha/2))
+    return abs(z) > critical_value
